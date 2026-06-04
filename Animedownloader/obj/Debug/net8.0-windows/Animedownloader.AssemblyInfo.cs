1e8c0f7961ddc888f6aad2fc4f861bd70f76e192
anointedthedeveloper <anointedthedeveloper@users.noreply.github.com>
2026-06-04 18:53:37 +0100

    Auto-update: sync downloaded episodes & changes

--- Animedownloader/obj/Debug/net8.0-windows/Animedownloader.AssemblyInfo.cs
+++ Animedownloader/obj/Debug/net8.0-windows/Animedownloader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Animedownloader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e94f7a9f20b288b53451e987579ae53795f949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b42fcdc668f0e7de1e480cd202ed3ad8532e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Animedownloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Animedownloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
